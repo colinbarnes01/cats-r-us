9ddf6ede8beafeb62c11341659a672e2e9605b11
Colin <tuf84887@temple.edu>
2017-02-14 13:38:02 -0500

    able to click buttons and add 2 balls to the screen

--- web/lab_03_jsIntro/bouncy_ball_framework.js
+++ web/lab_03_jsIntro/bouncy_ball_framework.js
@@ -3,12 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//"use strict";
+"use strict";
 
 function makeBallFramework() {
-    var ballFramework;
     
+    var ballFramework = {};
     
+
+    
+    ballFramework.makeBall = function (params) {
+        if (!params) {
+            alert("Please supply an input parameter to makeBall()");
+        }
+        if (!params.id) {
+            alert("Please supply an id in the parameter to makeBall()");
+        }
+        console.log('params.id in ballFramework.makeBall(): ', params.id);
+               
+        var ball = document.getElementById(params.id);
+        ball.style.top = params.top || "300px";
+        ball.style.left = params.left || "240px";
+        ball.style.position = params.position || "fixed";
+        ball.style.height = params.height || "276px";
+        ball.style.width = params.width || "276px";
+        //ball.style.backgroundColor = params.backgroundColor || "red";
+        ball.style.backgroundImage = "url('pictures/beach-ball.png')";
+        ball.style.borderRadius = "50%";
+        
+        return ball;
+    }
     
     return ballFramework;
 }
