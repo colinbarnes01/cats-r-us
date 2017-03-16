01794dccd7f75621328fbc063cc761d5827afb84
Colin Barnes <tuf84887@temple.edu>
2017-03-16 17:32:13 -0400

    refactored much of cat_animation.html into the FW.  walking buttons are now disabled after press

--- web/lab_03_jsIntro/cat_animation_framework.js
+++ web/lab_03_jsIntro/cat_animation_framework.js
@@ -9,8 +9,6 @@ function makeCatFramework() {
 
     var catFramework = {};
 
-
-
     catFramework.makeCat = function (params) {
         if (!params) {
             alert("Please supply an input parameter to makeCat()");
@@ -21,6 +19,7 @@ function makeCatFramework() {
 
         var cat = document.getElementById(params.id);
 
+        cat.id = params.id;
         cat.style.top = params.top || "200px";
         cat.style.left = params.left || "240px";
         cat.top = 200;
@@ -29,14 +28,26 @@ function makeCatFramework() {
         cat.style.position = params.position || "fixed";
         cat.style.height = params.height || "75px";
         cat.style.width = params.width || "75px";
-        //cat.style.backgroundColor = params.backgroundColor || "red";
         cat.style.backgroundImage = "url('pictures/cat_sprites.png')";
         cat.style.backgroundPosition = params.backgroundPosition || "-10px -200px";
         cat.style.backgroundSize = "1450%";
         cat.style.backgroundRepeat = "no-repeat";
-        //cat.style.borderRadius = "50%";
-        //cat.style.border = "red solid";
         cat.style.zIndex = "2";
+
+        // private parameters
+        cat.speed = 10;
+        cat.direction = "FORWARD";
+        cat.interval;   // interval is a js method for repeated actions, like changing sprites
+
+        cat.setSpeed = function (speed) {
+            catSpeed = speed;
+            //display();
+        };
+
+        cat.setDirection = function (direction) {
+            catDirection = direction;
+            //display();
+        };
 
         return cat;
     }
@@ -46,20 +57,55 @@ function makeCatFramework() {
         cat.style.backgroundPosition = position;
     }
 
-    catFramework.moveCat = function (id, speed) {
-        var cat = document.getElementById(id);
-        console.log('speed ', speed);
-        cat.left += speed;
-        //console.log('cat.left ', cat.left)
+    catFramework.moveCat = function (cat) {
+        //var cat = document.getElementById(cat.id);
+        cat.left += cat.speed;
         cat.style.left = cat.left + "px";
     }
-    
-    catFramework.changePosition = function(params) {
+
+    catFramework.changePosition = function (params) {
         var cat = document.getElementById(params.id);
         cat.style.left = params.left + "px";
         cat.style.top = params.top + "px";
     }
+
+    var interval01;
+    var interval02;
     
+    catFramework.animateCat = function (cat) {
+        console.log('cat: ' + cat);
+        if (interval01 == null) {
+            interval01 = setInterval(function () {
+                callChangeSprite(cat.id);
+                catFramework.moveCat(cat);
+            }, 200);
+            cat.interval = interval01;
+        } else {
+            interval02 = setInterval(function () {
+                callChangeSprite(cat.id);
+                catFramework.moveCat(cat);
+            }, 200);
+            cat.interval = interval02;
+        }
+        if (cat.id === 'cat01') {
+            document.getElementById("walkBtn01").disabled = true;
+        } else {
+            document.getElementById("walkBtn02").disabled = true;
+        }
+    };
+
+    catFramework.stopCat = function (cat) {
+        console.log('cat.id: ' + cat.id);
+        if (cat.id === 'cat01') {
+            document.getElementById("walkBtn01").disabled = false;
+        } else {
+            document.getElementById("walkBtn02").disabled = false;
+        }
+        clearInterval(cat.interval);
+    };
+
+
+
     return catFramework;
 }
 
