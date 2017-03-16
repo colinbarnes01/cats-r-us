5cd0e90ca9deaa1bd588296d74f93f90599ab489
Colin Barnes <tuf84887@temple.edu>
2017-03-16 13:57:24 -0400

    two animations working from framework calls

--- web/tutorial/animationFramework.js
+++ web/tutorial/animationFramework.js
@@ -14,22 +14,49 @@ function makeAnimationFW() {
 
     var animationFW = {};
 
-    animationFW.changeBgColor = function (id) {
-        var ele = document.getElementById(id);
-        console.log(ele);
-        ele.style.backgroundColor = "blue";
-    };
-
     animationFW.slideLeft = function (id) {
         console.log('inside slideLeft with id: ' + id);
         document.getElementById(id).classList.add('slider');
+        animationFW.appendSlideLeftCSSRules();
+    };
+
+    animationFW.appendSlideLeftCSSRules = function () {
+        var str = "<style>.slider{ animation-name:slideLeft; animation-duration:4s; } "
+                + "@keyframes slideLeft{ from{left:5px;} to{left:200px;} }</style>";
+
+        console.log(str);
+        $("head").append(str);
     };
 
     animationFW.changeColor = function (id) {
         console.log('inside changeColor with id: ' + id);
         document.getElementById(id).classList.add('colorChanger');
+        animationFW.appendChangeColorCSSRules();
+    };
+
+    animationFW.appendChangeColorCSSRules = function () {
+        var str = "<style>.colorChanger{ animation-name:changeColor; animation-duration:4s; } "
+                + "@keyframes changeColor{ from{background-color:yellow;} to{background-color:red;} }</style>";
+
+        console.log(str);
+        $("head").append(str);
     };
 
     return animationFW;
 }
 
+
+/*
+            .slider {
+                animation-name: slideLeft;
+                animation-duration: 4s;
+            }
+
+            @keyframes slideLeft {
+                from {
+                    left: 5px;
+                }
+                to {
+                    left: 200px;
+                }
+            }*/
