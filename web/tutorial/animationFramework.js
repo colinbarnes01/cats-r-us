278d69483bea23d9e8831a9fb74fc50ad4dcb57c
Colin Barnes <tuf84887@temple.edu>
2017-03-17 20:58:03 -0400

    restarting animations now takes into account html input fields

--- web/tutorial/animationFramework.js
+++ web/tutorial/animationFramework.js
@@ -14,36 +14,69 @@ function makeAnimationFW() {
 
     var animationFW = {};
 
-    animationFW.slideLeft = function (params) {
-        //console.log('inside slideLeft with params: ', params);
-        var ele = document.getElementById(params.id);
-        ele.classList.remove('slider');
-        document.getElementById(params.id).classList.add('slider');
-        animationFW.appendSlideLeftCSSRules(params);
+    animationFW.makeSlider = function (params) {
+        var slider = document.getElementById(params.id);
+        slider.leftStart = params.leftStart || 5;
+        slider.leftEnd = params.leftEnd || 200;
+        slider.duration = params.duration || 4;
+
+        console.log('slider created: ' + slider);
+
+        slider.slideLeft = function () {
+            slider.updateFields();
+            slider.classList.remove('slider');
+            slider.classList.add('slider');
+            slider.appendSlideLeftCSSRules();
+        };
+
+        slider.appendSlideLeftCSSRules = function () {
+            var str = "<style>.slider{ animation-name:slideLeft; animation-duration:" + slider.duration + "s; } "
+                    + "@keyframes slideLeft{ from{left:" + slider.leftStart + "px;} to{left:" + slider.leftEnd + "px;} }</style>";
+
+            console.log(str);
+            $("head").append(str);
+        };
+        
+        slider.updateFields = function() {
+                slider.leftStart = document.getElementsByName('leftStart')[0].value;
+                slider.leftEnd = document.getElementsByName('leftEnd')[0].value;
+                slider.duration = document.getElementsByName('slideDuration')[0].value;
+        }
+        return slider;
+    }
+
+
+    animationFW.makeColorChanger = function (params) {
+        var colorChanger = document.getElementById(params.id);
+        colorChanger.startColor = params.startColor || "#FFFF00";
+        colorChanger.endColor = params.endColor || "#FF0000";
+        colorChanger.duration = params.duration || 4;
+
+        colorChanger.changeColor = function () {
+            colorChanger.updateFields();
+            colorChanger.classList.remove('colorChanger');
+            colorChanger.classList.add('colorChanger');
+            colorChanger.appendChangeColorCSSRules();
+        };
+
+        colorChanger.appendChangeColorCSSRules = function () {
+            var str = "<style>.colorChanger{ animation-name:changeColor; animation-duration:" + colorChanger.duration + "s; } "
+                    + "@keyframes changeColor{ from{background-color:" + colorChanger.startColor + ";}"
+                    + "to{background-color:" + colorChanger.endColor + ";} }</style>";
+
+            $("head").append(str);
+        };
+
+        colorChanger.updateFields = function () {
+            colorChanger.startColor = document.getElementsByName('startColor')[0].value;
+            colorChanger.endColor = document.getElementsByName('endColor')[0].value;
+            colorChanger.duration = document.getElementsByName('colorChangeDuration')[0].value;
+
+        };
+        return colorChanger;
     };
 
-    animationFW.appendSlideLeftCSSRules = function (params) {
-        var str = "<style>.slider{ animation-name:slideLeft; animation-duration:" + params.duration + "s; } "
-                + "@keyframes slideLeft{ from{left:" + params.leftStart + "px;} to{left:" + params.leftEnd + "px;} }</style>";
 
-        //console.log(str);
-        $("head").append(str);
-    };
-
-    animationFW.changeColor = function (params) {
-        console.log('inside changeColor with params: ' + params);
-        document.getElementById(params.id).classList.add('colorChanger');
-        animationFW.appendChangeColorCSSRules(params);
-    };
-
-    animationFW.appendChangeColorCSSRules = function (params) {
-        var str = "<style>.colorChanger{ animation-name:changeColor; animation-duration:" + params.duration + "s; } "
-                + "@keyframes changeColor{ from{background-color:" + params.startColor + ";}"
-                + "to{background-color:" + params.endColor + ";} }</style>";
-
-        console.log(str);
-        $("head").append(str);
-    };
 
     animationFW.listenForButtonClickAndRestartAnimation = function () {
         $("#sliderButton").click(function () {
