de1f1171e00620f945eef50954f8508476c8d921
Colin Barnes <tuf84887@temple.edu>
2017-04-05 12:55:04 -0400

    triple lab part 2: got product update jsp and DBWrapper update function working

--- src/java/model/product/ProductDBWrapper.java
+++ src/java/model/product/ProductDBWrapper.java
@@ -92,7 +92,100 @@ public class ProductDBWrapper
         errorMsgs.imageUrl = ValidationUtils.stringValidationMsg(inputData.imageUrl, 10000, false); // 10,000 chars seems reasonable for a max url
         errorMsgs.description = ValidationUtils.stringValidationMsg(inputData.description, 45, false);
         errorMsgs.websiteUrl = ValidationUtils.stringValidationMsg(inputData.websiteUrl, 10000, false);
-         
+
+        return errorMsgs;
+    }
+
+    /**
+     * input parameters:
+     *
+     * inputData: an object that holds all the pre-validated fields that the
+     * user wants to update into the database. Remember that all fields in
+     * inputData are String type because this is PRE-VALIDATED data. dbc: an
+     * open DbConn database connection object.
+     *
+     * output parameter:
+     *
+     * If record is updated OK, return "" empty string. Otherwise, return a form
+     * level error message (e.g., if validation error, something like "please
+     * try again", or could be database error, or could be a programmer error
+     * msg).
+     */
+    public static ProductStringData update(ProductStringData productData, DbConn dbc)
+    {
+
+        ProductStringData errorMsgs = new ProductStringData();
+
+        System.out.println("In ProductDBWrapper.update() ready to update person with these values: " + productData.toString());
+
+        if (productData.productId == null)
+        {
+            errorMsgs.errorMsg = "Programmer error: for update, product Id should not be null.";
+            return errorMsgs;
+        }
+        if (productData.productId.length() == 0)
+        {
+            errorMsgs.errorMsg = "Programmer error: for update, person Id should not be empty string.";
+            return errorMsgs;
+        }
+
+        errorMsgs = validate(productData);
+        String formMsg = ""; // used for error messages
+        
+        if (errorMsgs.getCharacterCount() > 0)
+        {  // at least one field has an error, don't go any further.
+            System.out.println("Validation errors: " + errorMsgs.toString());
+            errorMsgs.errorMsg = "Please try again";
+            return errorMsgs;
+
+        } else
+        { // all fields passed validation
+            System.out.println("In ProductDBWrapper.update() passed validation");
+
+            // Start preparing SQL statement
+            formMsg = dbc.getErr(); // will be empty string if DB connection is OK.
+            if (formMsg.length() == 0)
+            { // db connection is good
+
+                // prepare the statement
+                String sql = "UPDATE product SET product_name=?, image_url=?, price=?, description=?, website_url=? WHERE product_id=?";
+
+                // PrepStatement is Sally's wrapper class for java.sql.PreparedStatement
+                // Only difference is that Sally's class takes care of encoding null 
+                // when necessary. And it also System.out.prints exception error messages.
+                PrepStatement pStatement = new PrepStatement(dbc, sql);
+
+                pStatement.setString(1, productData.productName);
+                pStatement.setString(2, productData.imageUrl);
+                pStatement.setInt(3, ValidationUtils.integerConversion(productData.price));
+                pStatement.setString(4, productData.description);
+                pStatement.setString(5, productData.websiteUrl);
+                pStatement.setInt(6, ValidationUtils.integerConversion(productData.productId));
+
+                System.out.println("ready to execute update, id is " + productData.productId);
+
+                // here the UPDATE is actually executed
+                int numRows = pStatement.executeUpdate();
+
+                // This will return empty string if all went well, else all error messages.
+                formMsg = pStatement.getErrorMsg();
+                System.out.println("Error msg from after executing the update: " + formMsg);
+
+                if (formMsg.length() == 0)
+                {
+                    if (numRows == 1)
+                    {
+                        formMsg = ""; // This means SUCCESS. Let the JSP page decide how to tell this to the user.
+                    } else
+                    {
+                        // probably never get here unless you forgot your WHERE clause and did a bulk sql update.
+                        formMsg = numRows + " records were updated when only 1 was expected.";
+                    }
+                    System.out.println("Number of records affected: " + numRows);
+                }
+            } // Db Connection is good - double check, JSP page should not send us a bad one... 
+        } // customerId is not null and not empty string.
+        errorMsgs.errorMsg = formMsg;
         return errorMsgs;
     }
 }
---
+++ web/tripleLab/apis/personUpdate.jsp
@@ -0,0 +1,44 @@
+<%@page contentType="application/json" pageEncoding="UTF-8"%> 
+<%@page language="java" import="dbUtils.DbConn"%> 
+<%@page language="java" import="model.product.*"%>
+
+<%@page language="java" import="com.google.gson.*" %>
+
+<%
+    Gson gson = new Gson();
+
+    // Create an empty ProductStringData object to hold all the possible field level error messages.
+    ProductStringData errorMsgs = new ProductStringData();
+
+    // Extract from the URL, the json-ized version of record they want to update in the database. 
+    String jsonUpdateData = request.getParameter("jsonData");
+
+    // Will need a database connection object.
+    DbConn dbc = new DbConn();
+
+    if (jsonUpdateData == null) {
+        errorMsgs.errorMsg = "Cannot update -- no data was received";
+    } else {
+
+        errorMsgs.errorMsg = dbc.getErr();
+        if (errorMsgs.errorMsg.length() == 0) { // means db connection is ok
+
+            System.out.println("personUpdate.jsp ready to update from this data: "
+                    + jsonUpdateData);
+
+            // Convert the json format (using the GSON object) to a POJO (plain old java object)
+            ProductStringData updateData = gson.fromJson(jsonUpdateData, ProductStringData.class);
+
+            // this method validates each field (putting any validation messages into 
+            // errorMsgs (ProductStringData object). 
+            errorMsgs = ProductDBWrapper.update(updateData, dbc); // this is the form level message 
+            
+            System.out.println("personUpdate.jsp found these errors: "
+                    + jsonUpdateData);
+        }
+    }
+    out.print(gson.toJson(errorMsgs).trim());
+
+    // prevent database connection leaks.
+    dbc.close();
+%>
